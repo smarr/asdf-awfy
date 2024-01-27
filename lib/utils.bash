7fcce91f1735a76f4c94c59480e4503b67eb4946
Stefan Marr <git@stefan-marr.de>
2024-01-27 21:59:26 +0000

    Remove unused code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for awfy.
-GH_REPO="https://github.com/smarr/are-we-fast-yet"
 TOOL_NAME="awfy"
 
 fail() {
@@ -39,12 +38,6 @@ fi
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
